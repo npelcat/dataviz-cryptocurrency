786328cf2ce92b145ac892e1c52d93964979c00a
npelcat <pelcat.nd@gmail.com>
2024-08-26 11:12:52 +0200

    first version of crypto-viz app

---
+++ src/components/Table.js
@@ -0,0 +1,161 @@
+import React, { useState } from "react";
+import TableLine from "./TableLine";
+import ToTop from "./ToTop";
+
+const Table = ({ coinsData }) => {
+  const [rangeNumber, setRangeNumber] = useState(100);
+  const [orderBy, setOrderBy] = useState("");
+
+  const tableHeader = [
+    "Prix",
+    "MarketCap",
+    "Volume",
+    "1h",
+    "1j",
+    "1s",
+    "1m",
+    "6m",
+    "1a",
+    "ATH",
+  ];
+
+  return (
+    <div className="table-container">
+      <ul className="table-header">
+        <div className="range-container">
+          <span>
+            Top{" "}
+            <input
+              type="text"
+              value={rangeNumber}
+              onChange={(e) => setRangeNumber(e.target.value)}
+            />
+          </span>
+          <input
+            type="range"
+            min="1"
+            max="250"
+            value={rangeNumber}
+            onChange={(e) => setRangeNumber(e.target.value)}
+          />
+          <ToTop />
+        </div>
+
+        {tableHeader.map((element) => (
+          <li key={element}>
+            <input
+              type="radio"
+              name="header-element"
+              id={element}
+              defaultChecked={
+                element === orderBy || element === orderBy + "reverse"
+                  ? true
+                  : false
+              }
+              onClick={() => {
+                if (orderBy === element) {
+                  setOrderBy(element + "reverse");
+                } else {
+                  setOrderBy(element);
+                }
+              }}
+            />
+            <label htmlFor={element}>{element}</label>
+          </li>
+        ))}
+      </ul>
+      {coinsData &&
+        coinsData
+          .slice(0, rangeNumber)
+          .sort((a, b) => {
+            switch (orderBy) {
+              case "Prix":
+                return b.current_price - a.current_price;
+              case "Volume":
+                return b.total_volume - a.total_volume;
+              case "MarketCap":
+                return b.market_cap - a.market_cap;
+              case "1h":
+                return (
+                  b.price_change_percentage_1h_in_currency -
+                  a.price_change_percentage_1h_in_currency
+                );
+              case "1j":
+                return (
+                  b.market_cap_change_percentage_24h -
+                  a.market_cap_change_percentage_24h
+                );
+              case "1s":
+                return (
+                  b.price_change_percentage_7d_in_currency -
+                  a.price_change_percentage_7d_in_currency
+                );
+              case "1m":
+                return (
+                  b.price_change_percentage_30d_in_currency -
+                  a.price_change_percentage_30d_in_currency
+                );
+              case "6m":
+                return (
+                  b.price_change_percentage_200d_in_currency -
+                  a.price_change_percentage_200d_in_currency
+                );
+              case "1a":
+                return (
+                  b.price_change_percentage_1y_in_currency -
+                  a.price_change_percentage_1y_in_currency
+                );
+              case "ATH":
+                return b.ath_change_percentage - a.ath_change_percentage;
+              case "#reverse":
+                return a.market_cap - b.market_cap;
+              case "Prixreverse":
+                return a.current_price - b.current_price;
+              case "Volumereverse":
+                return a.total_volume - b.total_volume;
+              case "MarketCapreverse":
+                return a.market_cap - b.market_cap;
+              case "1hreverse":
+                return (
+                  a.price_change_percentage_1h_in_currency -
+                  b.price_change_percentage_1h_in_currency
+                );
+              case "1jreverse":
+                return (
+                  a.market_cap_change_percentage_24h -
+                  b.market_cap_change_percentage_24h
+                );
+              case "1sreverse":
+                return (
+                  a.price_change_percentage_7d_in_currency -
+                  b.price_change_percentage_7d_in_currency
+                );
+              case "1mreverse":
+                return (
+                  a.price_change_percentage_30d_in_currency -
+                  b.price_change_percentage_30d_in_currency
+                );
+              case "6mreverse":
+                return (
+                  a.price_change_percentage_200d_in_currency -
+                  b.price_change_percentage_200d_in_currency
+                );
+              case "1areverse":
+                return (
+                  a.price_change_percentage_1y_in_currency -
+                  b.price_change_percentage_1y_in_currency
+                );
+              case "ATHreverse":
+                return a.ath_change_percentage - b.ath_change_percentage;
+              default:
+                return null;
+            }
+          })
+          .map((coin, index) => (
+            <TableLine coin={coin} key={coin.id} index={index} />
+          ))}
+    </div>
+  );
+};
+
+export default Table;
---
+++ src/components/TableLine.js
@@ -0,0 +1,78 @@
+import React, { useState } from "react";
+import PercentChange from "./PercentChange";
+import StarIcon from "./StarIcon";
+import CoinChart from "./CoinChart";
+
+const TableLine = ({ coin, index }) => {
+  const [showChart, setShowChart] = useState(false);
+
+  const priceFormater = (number) => {
+    if (Math.round(number).toString().length < 4) {
+      return new Intl.NumberFormat("us-US", {
+        minimumFractionDigits: 2,
+        maximumFractionDigits: 7,
+      }).format(number);
+    } else {
+      return number;
+    }
+  };
+
+  const mktCapFormater = (number) => {
+    let newNumber = String(number).split("").slice(0, -6);
+    return Number(newNumber.join(""));
+  };
+
+  return (
+    <div className="table-line">
+      <div className="infos-container">
+        <StarIcon coinId={coin.id} />
+        <p>{index + 1}</p>
+        <div className="img">
+          <img src={coin.image} height="20" alt="logo" />
+        </div>
+        <div className="infos">
+          <div
+            className="chart-img"
+            onMouseEnter={() => setShowChart(true)}
+            onMouseLeave={() => setShowChart(false)}
+          >
+            <img src="./assets/chart-icon.svg" alt="chart-icon" />
+            <div className="chart-container" id={coin.name}>
+              {showChart && <CoinChart coinId={coin.id} coinName={coin.name} />}
+            </div>
+          </div>
+          <h4>{coin.name}</h4>
+          <span>- {coin.symbol.toUpperCase()}</span>
+          <a
+            rel="noreferrer"
+            target="_blank"
+            href={
+              "https://www.coingecko.com/fr/pi%C3%A8ces/" +
+              coin.name.toLowerCase().replace(" ", "-").replace(" ", "-")
+            }
+          >
+            <img src="./assets/info-icon.svg" alt="info-icon" />
+          </a>
+        </div>
+      </div>
+      <p>{priceFormater(coin.current_price).toLocaleString()} $</p>
+      <p className="mktcap">
+        {mktCapFormater(coin.market_cap).toLocaleString()} M$
+      </p>
+      <p className="volume">{coin.total_volume.toLocaleString()} $</p>
+      <PercentChange percent={coin.price_change_percentage_1h_in_currency} />
+      <PercentChange percent={coin.market_cap_change_percentage_24h} />
+      <PercentChange percent={coin.price_change_percentage_7d_in_currency} />
+      <PercentChange percent={coin.price_change_percentage_30d_in_currency} />
+      <PercentChange percent={coin.price_change_percentage_200d_in_currency} />
+      <PercentChange percent={coin.price_change_percentage_1y_in_currency} />
+      {coin.ath_change_percentage > -3 ? (
+        <p>ATH !</p>
+      ) : (
+        <PercentChange percent={coin.ath_change_percentage} />
+      )}
+    </div>
+  );
+};
+
+export default TableLine;
